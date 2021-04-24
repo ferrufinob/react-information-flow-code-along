2514205aaadee98789be1331954443c1b7b6304e
Brenda Ferrufino <ferrufinob@gmail.com>
2021-04-24 17:37:49 -0400

    commit tree 94ae204727a17b332c769ca9a9f43a511ed690ab

--- src/Parent.js
+++ src/Parent.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import { getRandomColor } from './randomColorGenerator.js'
-import Child from './Child'
+import React, { Component } from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
+import Child from "./Child";
 
 class Parent extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
-      color: getRandomColor()
-    }
+      color: getRandomColor(),
+    };
   }
+
+  changeColor = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
 
   render() {
     return (
-      <div className="parent" style={{backgroundColor: this.state.color}}>
+      <div className="parent" style={{ backgroundColor: this.state.color }}>
         <Child />
         <Child />
       </div>
-    )
+    );
   }
 }
 
-export default Parent
+export default Parent;
